be7242580b20236aee6bff6351c76badfd57dd3c
stbmte <ateruhin388@gmail.com>
2022-09-18 13:19:58 +0300

    1.2

---
+++ src/main/java/com/example/javatech1_1/Javatech11Application.java
@@ -0,0 +1,13 @@
+package com.example.javatech1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javatech11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javatech11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javatech1_1/Javatech11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatech1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javatech11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
